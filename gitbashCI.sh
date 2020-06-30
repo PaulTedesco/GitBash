4f7c326ed3061a9f783a6b44bd0be1ac5e9dd778
Paul Tedesco <paul.tedesco@intercloud.com>
2020-06-30 16:06:10 +0200

    feat(master): New Feature

--- gitbashCI.sh
+++ gitbashCI.sh
@@ -22,7 +22,7 @@ if [[ ! -d .git ]]; then
   echo "go to https://github.com/new or https://gitlab.com/projects/new and create new project"
   echo -n "What is the clone url like this https://gitlab.com/steodec/TEST.git or git@gitlab.com:steodec/TEST.git : "
   read REPO
-  PATH_SCRIPT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd);
+  PATH_SCRIPT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
   git init
   git remote add origin $REPO
   if [[ ! -f README.md ]]; then
@@ -36,22 +36,32 @@ if [ -z $1 ]; then
   echo ""
   echo -n "Commit Message: "
   read COMMIT_MESSAGE
+  branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 else
   COMMIT_MESSAGE=$1
 fi
 if [ -z $2 ]; then
-  echo -e "\e[91mERROR\e[39m: No branch."
-  echo ""
-  echo -n "Branch: "
-  read BRANCH
+  BRANCH=$branch
 else
   BRANCH=$2
 fi
+select item in "- Feature -" "- Fix -"; do
+  case $REPLY in
+  1)
+    type="feat"
+    break
+    ;;
+  2)
+    type="fix"
+    break
+    ;;
+  esac
+done
 git add .
-git commit -m "$COMMIT_MESSAGE"
+git commit -m "$type($branch): $COMMIT_MESSAGE"
 git push origin $BRANCH
 
 echo -e "\e[92mSUCCESS\e[39m: Commit push on branch $BRANCH with this message $COMMIT_MESSAGE"
 echo ""
 
-#EOF
+EOF
